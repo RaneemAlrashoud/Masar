a224cb9a688190837d630cfa019eacdbe7b5f0b6
Raneem AlRashoud <439200695@student.ksu.edu.sa>
2022-06-22 16:01:05 +0300

    Setup

---
+++ Masar/Masar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Masar
+//
+//  Created by Raneem AlRashoud on 23/11/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
